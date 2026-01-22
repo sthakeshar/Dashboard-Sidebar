01d09647098c8d67250e8e2e84932231d9b742c0
sthakeshar <shresthakeshar291@gmail.com>
2026-01-22 20:53:34 +0900

    sidebar collapse added

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+const toggleButton=document.getElementById("toggle-btn")
+const sidebar=document.getElementById("sidebar")
+
+console.log(document.getElementById('sidebar'));
+function toggleSidebar(){
+    toggleButton.classList.toggle('rotate')
+    sidebar.classList.toggle('close')
+}
 function toggleSubMenu(button){
     button.nextElementSibling.classList.toggle('show')
     button.classList.toggle('rotate')

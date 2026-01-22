44388b3af0fe155c01e2f5e42e63c38ce09f3352
sthakeshar <shresthakeshar291@gmail.com>
2026-01-22 22:59:27 +0900

    project completed

--- script.js
+++ script.js
@@ -5,8 +5,24 @@ console.log(document.getElementById('sidebar'));
 function toggleSidebar(){
     toggleButton.classList.toggle('rotate')
     sidebar.classList.toggle('close')
+    closeAllSubMenus()
 }
 function toggleSubMenu(button){
+    if(!button.nextElementSibling.classList.contains('show')){
+        closeAllSubMenus()
+    }
     button.nextElementSibling.classList.toggle('show')
     button.classList.toggle('rotate')
+
+    if(sidebar.classList.contains('close')){
+        button.classList.toggle('rotate')
+        sidebar.classList.toggle('close')
+    }
+}
+
+function closeAllSubMenus(){
+     Array.from(sidebar.getElementsByClassName('show')).forEach(ul =>{
+        ul.classList.remove('show')
+        ul.previousElementSibling.classList.remove('rotate')
+    } )
 }
